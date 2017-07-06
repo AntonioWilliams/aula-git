03a7bfa09f034d0eda4355623959879efa8f4177
Antonio Williams <toni.maleta@gmail.com>
2017-07-06 10:49:06 -0300

    Mudança Itil.

--- src/br/curso/leitor/Importador.java
+++ src/br/curso/leitor/Importador.java
@@ -5,11 +5,18 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import br.curso.modelo.Cliente;
+
 public class Importador {
 	
 	public<Gasto> Importador(InputStream entrada) throws ParseException{
 		Scanner leitor = new Scanner(entrada);
 		ArrayList<Gasto> gastos = new ArrayList<Gasto>();
+		Cliente cliente = new Cliente();
+		
+		if(cliente.getNome()=="cesar"){
+			
+		}
 		
 		/*
 		 * com essa nova versao ficara muito facil
