a059c3aa7d27327a4685887a0c41ad90898fcf5a
Antonio Williams <toni.maleta@gmail.com>
2017-07-06 10:42:46 -0300

    commited

--- src/br/curso/leitor/AmebaRepository.java
+++ src/br/curso/leitor/AmebaRepository.java
@@ -1,10 +1,14 @@
 package br.curso.leitor;
 
 public class AmebaRepository {
-	
+	int [] data = {0, 6, 67};
 	public void salvar(){
 		System.out.println("TESTE COMO FAZER");
 		System.out.println("TESTE COMO FAZER");
 		System.out.println("TESTE COMO FAZER");
+		
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(data[i]);
+		}
 	}
 }
