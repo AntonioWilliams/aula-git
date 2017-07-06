a1fa16e6a8a2ff63c6f82500251cd309fc5c18fd
Antonio Williams <toni.maleta@gmail.com>
2017-07-06 10:34:40 -0300

    Local Brach

--- src/br/curso/leitor/AmebaRepository.java
+++ src/br/curso/leitor/AmebaRepository.java
@@ -3,6 +3,8 @@ package br.curso.leitor;
 public class AmebaRepository {
 	
 	public void salvar(){
-		
+		System.out.println("TESTE COMO FAZER");
+		System.out.println("TESTE COMO FAZER");
+		System.out.println("TESTE COMO FAZER");
 	}
 }
